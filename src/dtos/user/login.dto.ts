9895f0619e174bb03a234289c4992ea4a8432c10
henriquedias420 <henriquediasper@hotmail.com>
2023-08-25 12:29:43 -0300

    :sparkles: feat: Endpoint de login

--- src/dtos/user/login.dto.ts
+++ src/dtos/user/login.dto.ts
@@ -9,7 +9,9 @@ export interface LoginOutputDTO {
   token: string;
 }
 
-export const LoginSchema = z.object({
-  email: z.string().email(),
-  password: z.string().min(4),
-});
+export const LoginSchema = z
+  .object({
+    email: z.string().email(),
+    password: z.string().min(4),
+  })
+  .transform((data) => data as LoginInputDTO);
