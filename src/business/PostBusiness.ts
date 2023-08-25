76b4b6e2a9e267298fec682ab3c0717b10fd1279
henriquedias420 <henriquediasper@hotmail.com>
2023-08-25 18:58:53 -0300

    :sparkles: feat: Endpoint para criar um post

--- src/business/PostBusiness.ts
+++ src/business/PostBusiness.ts
@@ -1,4 +1,10 @@
 import { PostDataBase } from "../database/PostDatabase";
+import {
+  CreatePostInputDTO,
+  CreatePostOutputDTO,
+} from "../dtos/post/createPost.dto";
+import { UnauthorizedError } from "../errors/UnauthorizedError";
+import { Post } from "../models/Post";
 import { IdGenerator } from "../services/IdGenerator";
 import { TokenManager } from "../services/TokenManager";
 
@@ -8,4 +14,39 @@ export class PostBusiness {
     private idGenerator: IdGenerator,
     private tokenManager: TokenManager
   ) {}
+
+  public createPost = async (
+    input: CreatePostInputDTO
+  ): Promise<CreatePostOutputDTO> => {
+    const { token, content } = input;
+
+    const payload = this.tokenManager.getPayload(token);
+
+    if (!payload) {
+      throw new UnauthorizedError("Token inválido");
+    }
+
+    const id = this.idGenerator.generate();
+
+    const post = new Post(
+      id,
+      content,
+      0,
+      0,
+      0,
+      new Date().toISOString(),
+      payload.id,
+      payload.name
+    );
+
+    const postDB = post.toDBModel();
+
+    await this.postDatabase.insertPost(postDB);
+
+    const output: CreatePostOutputDTO = {
+      message: "Post criado com sucesso",
+    };
+
+    return output;
+  };
 }
