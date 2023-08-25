6ccf06c802738f77ec0f69d0c0c5387d7b6d2db8
henriquedias420 <henriquediasper@hotmail.com>
2023-08-25 11:41:41 -0300

    :sparkles: feat: Endpoint de signup

--- src/dtos/user/signup.dto.ts
+++ src/dtos/user/signup.dto.ts
@@ -10,8 +10,10 @@ export interface SignupOutputDTO {
   token: string;
 }
 
-export const SignupSchema = z.object({
-  name: z.string().min(2),
-  email: z.string().email(),
-  password: z.string().min(4),
-});
+export const SignupSchema = z
+  .object({
+    name: z.string().min(2),
+    email: z.string().email(),
+    password: z.string().min(4),
+  })
+  .transform((data) => data as SignupInputDTO);
