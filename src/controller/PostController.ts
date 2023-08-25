514cde52c75bd11d8ada4c6588e9f16f3cb12653
henriquedias420 <henriquediasper@hotmail.com>
2023-08-25 20:50:46 -0300

    :sparkles: feat: Endpoint para pegar todos os posts

--- src/controller/PostController.ts
+++ src/controller/PostController.ts
@@ -3,6 +3,7 @@ import { PostBusiness } from "../business/PostBusiness";
 import { ZodError } from "zod";
 import { BaseError } from "../errors/BaseError";
 import { CreatePostSchema } from "../dtos/post/createPost.dto";
+import { GetPostSchema } from "../dtos/post/getPost.dto";
 
 export class PostController {
   constructor(private postBusiness: PostBusiness) {}
@@ -26,6 +27,28 @@ export class PostController {
         res.status(error.statusCode).send(error.message);
       } else {
         res.status(500).send("Erro inesperado");
+      }
+    }
+  };
+
+  public getPosts = async (req: Request, res: Response) => {
+    try {
+      const input = GetPostSchema.parse({
+        token: req.headers.authorization,
+      });
+
+      const output = await this.postBusiness.getPosts(input);
+
+      res.status(200).send(output);
+    } catch (error) {
+      console.log(error);
+
+      if (error instanceof ZodError) {
+        res.status(400).send(error.issues);
+      } else if (error instanceof BaseError) {
+        res.status(error.statusCode).send(error.message);
+      } else {
+        res.status(500).send("Unexpected Error");
       }
     }
   };
